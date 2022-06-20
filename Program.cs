264411bf1372b63614c5ebf415d79c7007cc5547
Brooks Adams <aba1421@jagmail.southalabama.edu>
2022-06-19 21:49:24 -0500

    first commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Syntax_Exercise
     {
         static void Main(string[] args)
         {
-            var answer = 4;
+            //var answer = 4;
             //string response;
             //if (answer < 9)
             //{
@@ -16,7 +16,8 @@ namespace Syntax_Exercise
             //{
             //    response = answer + " is greater than or equal to nine";
             //}
-
+            
+            var answer = 4;
             var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
         }
     }
